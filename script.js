23e274f4972159affa93a493517f15d42edd9038
Emanuel Joness <itzemanuelj@gmail.com>
2022-11-02 00:29:53 -0400

    inital commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// An alternative to using && to test multiple conditions in a single if statement is ______ ifs.
+let nesterd;
+
+// You communicate the nesting to JavaScript with the placement of what two characters? Don't type a space between them.
+if (a = b) {
+  console.log('a');
+}
+
+// This is the first line of an if statement.
+if (a !== b) {
+// Enter the first 6 characters of the next line, which is a second-level if.
+}
